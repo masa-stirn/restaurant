dc39d57050f52cdd9cea1219d44e2b7dec8cab7a
Masa Wu <masastirn23@gmail.com>
2018-02-08 14:33:57 +0100

    modal img loading added

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const imgLink = "http://kea-alt-del.dk/t5/site/imgs/";
 const catLink = "http://kea-alt-del.dk/t5/api/categories";
 const productLink = "http://kea-alt-del.dk/t5/api/product?id=";
 
-
 fetch(catLink).then(result => result.json()).then(catData => catFunction(catData));
 
 function getPlist(link) {
@@ -27,7 +26,7 @@ function catFunction(myData) {
         const a = document.createElement("a");
         a.textContent = elem;
         a.href = "#";
-        a.addEventListener("click", ()=>filter(elem));
+        a.addEventListener("click", () => filter(elem));
         filterDiv.appendChild(a);
 
     });
@@ -40,18 +39,17 @@ function show(myData) {
         const container = document.querySelector("#" + elem.category);
         const clone = prodTemplate.cloneNode(true);
         clone.querySelector(".read-more").addEventListener("click", () => {
-            fetch(productLink + elem.id).then(result=>result.json()).then(product=>showDetails(product));
+            fetch(productLink + elem.id).then(result => result.json()).then(product => showDetails(product));
             modal.classList.remove("hide");
         });
         clone.querySelector("img").src = imgLink + "small/" + elem.image + "-sm.jpg";
         clone.querySelector("img").title = elem.shortdescription;
         clone.querySelector("img").addEventListener("click", () => {
-            fetch(productLink + elem.id).then(result=>result.json()).then(product=>showDetails(product));
+            fetch(productLink + elem.id).then(result => result.json()).then(product => showDetails(product));
             model.classList.remove("hide");
         });
         clone.querySelector("h3").textContent = elem.name;
-        clone.querySelector("h4 span").textContent =elem.price;
-
+        clone.querySelector("h4 span").textContent = elem.price;
 
         if (elem.vegetarian === true && elem.category != "drinks") {
             clone.querySelector(".green").classList.remove("hide");
@@ -59,7 +57,7 @@ function show(myData) {
         if (elem.soldout === true) {
             clone.querySelector(".orange").classList.remove("hide");
         }
-        if (elem.alcohol) {// zero is false = implicit false
+        if (elem.alcohol) { // zero is false = implicit false
             clone.querySelector(".alco").classList.remove("hide");
             const newImage = document.createElement("img");
             newImage.setAttribute("src", "imgs/cocktail-26182.svg");
@@ -68,7 +66,7 @@ function show(myData) {
             clone.querySelector(".alco-div").prepend(newImage);
         }
         if (elem.discount > 0) {
-            const newPrice = Math.round(elem.price - elem.price * elem.discount/100);
+            const newPrice = Math.round(elem.price - elem.price * elem.discount / 100);
             clone.querySelector(".discount").textContent = "Now only " + newPrice + " DKK";
             clone.querySelector(".red").classList.remove("hide");
             clone.querySelector(".discount").classList.remove("hide");
@@ -78,23 +76,30 @@ function show(myData) {
         container.appendChild(clone);
     })
 }
-
-function filter(myFilter){
-            document.querySelectorAll("main .cat-section").forEach(section=>{
-                if (section.id == myFilter || myFilter == "menu"){
-                    section.classList.remove("hide");
-                }
-                else{
-                    section.classList.add("hide");
-                }
-            })
+function filter(myFilter) {
+    document.querySelectorAll("main .cat-section").forEach(section => {
+        if (section.id == myFilter || myFilter == "menu") {
+            section.classList.remove("hide");
+        } else {
+            section.classList.add("hide");
         }
-
-function showDetails(product){
-   modal.querySelector(".large-name").textContent=product.name;
-   modal.querySelector(".large-description").textContent=product.longdescription;
-   modal.querySelector(".large-image").src = imgLink + "large/" + product.image + ".jpg";
-            }
+    })
+}
+function showDetails(product) {
+    modal.querySelector(".large-name").textContent = product.name;
+    if (product.longdescription) {
+        modal.querySelector(".large-description").textContent = product.longdescription;
+    } else {
+        modal.querySelector(".large-description").textContent = product.shortdescription;
+    }
+    modal.querySelector(".large-image").src = imgLink + "large/" + product.image + ".jpg";
+    setTimeout(function () {
+        modal.querySelector(".large-image").style.opacity = 1;
+    }, 300);
+}
 document.querySelector(".close").addEventListener("click", x => {
-           modal.classList.add("hide");
-        });
+    modal.classList.add("hide");
+    modal.querySelector(".large-name").textContent = "Loading...";
+    modal.querySelector(".large-description").textContent = "";
+    modal.querySelector(".large-image").style.opacity = 0;
+});
